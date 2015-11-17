ee7631e3500af07c3dc51eefb7e72a4483cf49d3
user <user@user-Komputer>
2015-11-18 00:57:46 +0100

    searchForm: deleted required warrning

--- src/view/searchForm.php
+++ src/view/searchForm.php
@@ -1,6 +1,6 @@
 <form class="navbar-form" role="search">
     <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" name="q" required value="<?= getSearchQuery() ?>">
+        <input type="text" class="form-control" placeholder="Search" name="q" value="<?= getSearchQuery() ?>">
         <div class="input-group-btn">
             <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
         </div>
