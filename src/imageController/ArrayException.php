323425d0b2698bbec4dfe3f06dabc5d0a2ad36fb
xandros15 <user@user-Komputer>
2015-12-06 01:13:23 +0100

    core: add exception class "ArrayException"

---
+++ src/imageController/ArrayException.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app;
+
+use Exception;
+
+class ArrayException extends Exception
+{
+
+    /**
+     * Constructor.
+     * @param array $message error messages
+     */
+    public function __construct($message = null)
+    {
+        if (is_array($message)) {
+            $message = $this->flatten($message);
+        }
+        parent::__construct($message);
+    }
+
+    private function flatten($array)
+    {
+        $return = '';
+        array_walk_recursive(
+            $array, function ($a) use (&$return) {
+            $return .= $a . '<br>';
+        }
+        );
+        return $return;
+    }
+}
