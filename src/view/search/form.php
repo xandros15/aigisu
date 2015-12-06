99d8ab9c61379aba606b634d192f27d77fb2ca9d
xandros15 <user@user-Komputer>
2015-12-06 23:59:34 +0100

    View searchform: better help for search

--- src/view/search/form.php
+++ src/view/search/form.php
@@ -1,13 +1,17 @@
 <form class="navbar-form" role="search">
+    <div class="col-xs-12">
     <div class="input-group">
         <input type="text" class="form-control" placeholder="Search" name="q" value="<?= getSearchQuery() ?>">
         <div class="input-group-btn">
             <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
         </div>
     </div>
+    </div>
+    <div class="col-xs-12">
+        <p class="help-block">
+            You can use in search form: <code>&lt;namespace&gt;:&lt;value&gt;</code><br>
+            avaible namespaces: <code>name, rarity, orginal</code><br>
+            example: <code>iris rarity:gold</code><br>
+        </p>
+    </div>
 </form>
-<div>
-    <p>you can use in search form: namespace:value</p>
-    <p>avaible namespaces: name(default), id, rarity, orginal(japanese)</p>
-    <p>example: iris rarity:gold</p>
-</div>
