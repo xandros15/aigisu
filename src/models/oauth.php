f7f2596129cbdcf264f1fcb990095541e2fad220
xandros15 <user@user-Komputer>
2015-12-07 01:41:56 +0100

    init oauth model

---
+++ src/models/oauth.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace models;
+
+use RedBeanPHP\R;
+use RedBeanPHP\OODBBean;
+
+class oauth
+{
+    const SESSION_NAME = 'oauth';
+
+    public $errorLog = [];
+    private $token;
+
+    public static function tableName()
+    {
+        return 'oauth';
+    }
+
+    public static function isLogged()
+    {
+        return (isset($_SESSION[self::SESSION_NAME]) && $_SESSION[self::SESSION_NAME]);
+    }
+
+    public function run()
+    {
+        if ($this->startSession() && $this->validatePin()) {
+            $this->login();
+        }
+    }
+
+    public function getErrorLog()
+    {
+        return $this->errorLog;
+    }
+
+    private function startSession()
+    {
+        return (session_status() == PHP_SESSION_NONE) ? session_start() : false;
+    }
+
+    public static function logout()
+    {
+        $_SESSION = [];
+        return session_destroy();
+    }
+
+    private function login()
+    {
+        $_SESSION[self::SESSION_NAME] = true;
+        $_SESSION['token']            = $this->token;
+    }
+
+    private function validatePin()
+    {
+        global $query;
+        if (empty($query->post->pin) || strlen($query->post->pin) > 32) {
+            return false;
+        }
+        $pin     = $query->post->pin;
+        if (($results = R::find(self::tableName(), ' pin = ? ', [$pin]))) {
+            /* @var $result OODBBean */
+            $result = reset($results);
+            if ($this->isTimeout($result->timeout)) {
+                $this->errorLog[] = 'Pin is outdated';
+                R::trash($result);
+                return false;
+            }
+            $this->token = $result->token;
+            return true;
+        }
+        $this->errorLog[] = 'Wrong pin';
+        return false;
+    }
+
+    private function isTimeout($time)
+    {
+        return (time() - $time < 0);
+    }
+}
