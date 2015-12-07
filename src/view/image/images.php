19baffbb44e9e4ce122e57e778b302b97bff9fef
xandros15 <user@user-Komputer>
2015-12-08 00:56:31 +0100

    Front: center server name

--- src/view/image/images.php
+++ src/view/image/images.php
@@ -8,7 +8,7 @@ $labels   = $imageSet->getAllImages();
 ?>
 <?php foreach ($labels as $label => $images): ?>
     <ul class="list-unstyled <?= $label ?> col-xs-12 col-sm-6 <?= count($labels) > 1 ? : 'col-sm-offset-3' ?>">
-        <h2>#<?= $label ?></h2>
+        <h2 class="text-center">#<?= $label ?></h2>
         <?php foreach ($images as $i => $image): ?>
             <li style="display: inline-block; position: relative;">
                 <span style="position: absolute; top: 10px; left: 10px; font-size: 20px;">#<?= $image->id ?></span>
