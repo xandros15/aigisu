cddae34b2f1ee4d9c4a59ee292921607afc6aebb
xandros15 <user@user-Komputer>
2015-12-15 23:53:12 +0100

    view: better layout in new system

---
+++ src/view/layout/main.php
@@ -0,0 +1,36 @@
+<?php
+
+use app\alert\Alert;
+use app\core\View;
+
+/* @var $this View */
+/* @var $content string */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?= $this->title ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" rel="stylesheet" >
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/main.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="wrap">
+            <main class="<?= $this->containerClass ?>">
+                <?= Alert::display(); ?>
+                <?= $content ?>
+            </main>
+        </div>
+        <footer class="footer text-center">
+            <p>&copy; xandros. Images and media relating to Millennium War Aigis are property of Nutaku.net and DMM.com</p>
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+        <script>globalUrl = '<?= SITE_URL ?>';</script>
+        <script src="/js/updateAjax.js"></script>
+        <script src="/js/blockDisabledLinks.js"></script>
+        <script src="/js/openImages.js"></script>
+    </body>
+</html>
