c0c0ccd2fde28c3c278a6077b66a2186fb36e3d3
xandros15 <user@user-Komputer>
2015-12-15 19:31:13 +0100

    core: setup basic function on view and controllers

--- src/components/core/Controller.php
+++ src/components/core/Controller.php
@@ -2,7 +2,20 @@
 
 namespace app\core;
 
+use app\core\View;
+
 class Controller
 {
+    /** @var View */
+    private $view;
 
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    public function render($view, $params = [])
+    {
+        return $this->view->render($view, $params);
+    }
 }
--- src/components/core/View.php
+++ src/components/core/View.php
@@ -4,5 +4,17 @@ namespace app\core;
 
 class View
 {
+    const VIEW_DIR = VIEW_DIR;
 
+    public $title;
+
+    public function render($view, $params = [])
+    {
+        $filename = str_replace('/', DIRECTORY_SEPARATOR, $view);
+        ob_start();
+        ob_implicit_flush(false);
+        extract($params, EXTR_OVERWRITE);
+        require (self::VIEW_DIR . $filename . '.php');
+        return ob_get_clean();
+    }
 }
