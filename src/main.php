49b6a5f27812da61d82d37ee037e9c283008d67d
xandros15 <user@user-Komputer>
2015-12-16 19:12:21 +0100

    core: clean main class

--- src/main.php
+++ src/main.php
@@ -6,9 +6,6 @@ $query = (object) [
 ];
 
 use RedBeanPHP\Facade as R;
-use app\upload\UploadImages;
-use models\Image;
-use models\Unit;
 use Slim\App as Slim;
 use Slim\Container;
 use controller\ImageController;
@@ -22,15 +19,7 @@ class Main
 {
     static $app;
 
-    function dbconnect()
-    {
-        require CONFIG_DIR . 'db.config.php';
-        R::setup('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
-        R::debug(DEBUG);
-        R::freeze();
-    }
-
-    function bootstrap()
+    public function bootstrap()
     {
         static::$app = $this;
         $this->configuration();
@@ -40,39 +29,7 @@ class Main
         $this->goSlimRoute();
     }
 
-    function createSessions()
-    {
-        $alert = new Alert();
-        $alert->init();
-        $oauth = new Oauth();
-        $oauth->run();
-    }
-
-    function setAutoloader()
-    {
-        require_once ROOT_DIR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-    }
-
-    function renderPhpFile($_file_, $_params_ = [])
-    {
-        $filename = str_replace('/', DIRECTORY_SEPARATOR, $_file_);
-        ob_start();
-        ob_implicit_flush(false);
-        extract($_params_, EXTR_OVERWRITE);
-        require (VIEW_DIR . $filename . '.php');
-        return ob_get_clean();
-    }
-
-    function configuration()
-    {
-        defined('SITE_URL') || define('SITE_URL', 'http://aigisu.pl/');
-        defined('CONFIG_DIR') || define('CONFIG_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
-        defined('VIEW_DIR') || define('VIEW_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
-        defined('MAX_ROWS') || define('MAX_ROWS', 30);
-        defined('DEBUG') || define('DEBUG', 0);
-    }
-
-    function generateLink(array $options)
+    public function generateLink(array $options)
     {
         global $query;
         if (!isset($query->get)) {
@@ -86,12 +43,12 @@ class Main
         return '?' . http_build_query($get);
     }
 
-    function reverseGet(stdClass $get, $name, $value)
+    public function reverseGet(stdClass $get, $name, $value)
     {
         return ($name == 'sort' && isset($get->{$name}) && strpos($get->{$name}, '-') !== 0) ? '-' . $value : $value;
     }
 
-    function getSearchQuery()
+    public function getSearchQuery()
     {
         global $query;
         if (empty($query->get->q)) {
@@ -100,7 +57,37 @@ class Main
         return $query->get->q;
     }
 
-    function goSlimRoute()
+    private function configuration()
+    {
+        defined('SITE_URL') || define('SITE_URL', 'http://aigisu.pl/');
+        defined('CONFIG_DIR') || define('CONFIG_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
+        defined('VIEW_DIR') || define('VIEW_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
+        defined('MAX_ROWS') || define('MAX_ROWS', 30);
+        defined('DEBUG') || define('DEBUG', 0);
+    }
+
+    private function dbconnect()
+    {
+        require CONFIG_DIR . 'db.config.php';
+        R::setup('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+        R::debug(DEBUG);
+        R::freeze();
+    }
+
+    private function createSessions()
+    {
+        $alert = new Alert();
+        $alert->init();
+        $oauth = new Oauth();
+        $oauth->run();
+    }
+
+    private function setAutoloader()
+    {
+        require_once ROOT_DIR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+    }
+
+    private function goSlimRoute()
     {
         $controllers               = [
             ImageController::class => function ($c) {
