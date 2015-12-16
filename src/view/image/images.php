9891ff2143b9f16bf040d80745f4dd75e7f4e0b9
xandros15 <user@user-Komputer>
2015-12-16 02:26:44 +0100

    View Images: sync to new MVC system

--- src/view/image/images.php
+++ src/view/image/images.php
@@ -1,20 +1,21 @@
 <?php
 
 use models\Images;
+use app\core\View;
 
-global $query;
+/* @var $this View */
 /* @var $imageSet Images */
-$imageSet = Images::imagesByUnit($query->get->image);
-$images   = $imageSet->getSortedImages();
+$this->setTitle('images');
+$this->setContainerClass('container-fluid');
 ?>
 <?php foreach ($images as $serverName => $server): ?>
-    <ul class="list-unstyled <?= $serverName ?> col-xs-12 col-sm-6<?= ($imageSet->length > 2) ? '' : ' col-sm-offset-3' ?>">
+    <ul class="list-unstyled <?= $serverName ?> col-xs-12 col-sm-6">
         <h2 class="text-center">#<?= $serverName ?></h2>
-        <?php foreach ($server as $scene => $image): ?>
+    <?php foreach ($server as $scene => $image): ?>
             <li style="display: inline-block; position: relative;">
                 <span style="position: absolute; top: 10px; left: 10px; font-size: 20px;">#<?= $image->id ?></span>
                 <img id="<?= $image->id ?>" alt="<?= $serverName . $scene ?>" style="max-width: 100%;" src="<?= Images::createImagelink($image->id) ?>">
             </li>
-        <?php endforeach; ?>
+    <?php endforeach; ?>
     </ul>
 <?php endforeach; ?>
