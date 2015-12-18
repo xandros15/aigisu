8f75ea48406fbce1af053c4382fa6963893c64ef
xandros15 <user@user-Komputer>
2015-12-18 18:51:34 +0100

    core: clean index script

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ defined('ROOT_DIR') || define('ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR);
 
 require_once ROOT_DIR . 'src' . DIRECTORY_SEPARATOR . 'main.php';
 
-$main = new Main;
+$main = new Main();
 
 $main->bootstrap();
 $main->run();
