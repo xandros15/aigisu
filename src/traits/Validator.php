dafc4c527e16548ecd93c25f94122f01cf206d2d
xandros15 <user@user-Komputer>
2015-12-21 03:21:27 +0100

    validator: better validate

--- src/traits/Validator.php
+++ src/traits/Validator.php
@@ -3,6 +3,8 @@
 namespace traits;
 
 use Illuminate\Support\MessageBag;
+use Illuminate\Database\Eloquent\Model;
+use app\alert\Alert;
 use Main;
 
 trait Validator
@@ -10,19 +12,22 @@ trait Validator
     /** @var  MessageBag */
     public $errors;
 
-    public function validate($data)
+    public function validate()
     {
+        /* @var $this Model */
         if (!method_exists($this, 'rules')) {
             return true;
         }
-        $validator = Main::$app->connection->validator->make($data, $this->rules());
+
+        $validator = Main::$app->connection->validator->make($this->getAttributes(), $this->rules());
 
         $this->errors = $validator->errors();
-        return $validator->passes();
-    }
-
-    public function getErrors()
-    {
-        return $this->errors;
+        if ($validator->passes()) {
+            return true;
+        }
+        foreach ($this->errors as $error) {
+            Alert::add($error, Alert::ERROR);
+        }
+        return false;
     }
 }
