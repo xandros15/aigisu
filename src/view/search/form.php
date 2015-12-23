c9a8418aeb583086e85e5fda1e83780593190595
xandros15 <user@user-Komputer>
2015-12-23 19:12:06 +0100

    View: add link to oauth

--- src/view/search/form.php
+++ src/view/search/form.php
@@ -11,6 +11,7 @@ use controller\UnitController;
                 <button type="button" class="btn btn-default" data-placement="bottom" data-toggle="popover" title="how to use search form" data-container="body" data-content="You can use in search form: &lt;namespace&gt;:&lt;value&gt; || avaible namespaces: name, rarity, original || example: iris rarity:gold || You can also use: male(for male units), dmm(for only dmm units), nutaku(for nutaku units)">
                     <span class="glyphicon glyphicon-question-sign"></span>
                 </button>
+                <a href="<?= Main::$app->router->pathFor('oauth') ?>" role="button" class="btn btn-default"><span class="glyphicon glyphicon-log-in"></span></a>
             </div>
         </div>
     </form>
