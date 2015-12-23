679a873ce13d19ddf04f09a7d4ce508e94d57785
xandros15 <user@user-Komputer>
2015-12-23 20:21:39 +0100

    core fe: implement polyfiler validator

--- src/view/layout/main.php
+++ src/view/layout/main.php
@@ -37,5 +37,7 @@ use app\core\View;
         </script>
         <script src="/js/blockDisabledLinks.js"></script>
         <script src="/js/openImages.js"></script>
+        <script src="https://afarkas.github.io/webshim/js-webshim/minified/polyfiller.js"></script>
+        <script src="/js/html5Validator.js"></script>
     </body>
 </html>
