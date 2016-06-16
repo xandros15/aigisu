7c5771ae253792706f91b8d73366cd2a0d7417da
xandros15 <xandros92@gmail.com>
2016-06-16 14:40:25 +0200

    set new container

--- src/main.php
+++ src/main.php
@@ -1,6 +1,8 @@
 <?php
 
+use app\core\View;
 use Slim\App as Slim;
+use Slim\Container;
 use Slim\Router;
 use Slim\Http\Request;
 use app\alert\Alert;
@@ -8,7 +10,7 @@ use app\core\Configuration;
 use app\slim\SlimConfig;
 use app\core\Connection;
 
-class Main
+class Main extends \Slim\App
 {
     /** @var Main */
     static $app;
@@ -28,6 +30,12 @@ class Main
     /** @var Slim */
     private $slim;
 
+    public function __construct()
+    {
+        $this->bootstrap();
+        parent::__construct($this->createContainer());
+    }
+
     public function bootstrap()
     {
         static::$app = $this;
@@ -38,14 +46,36 @@ class Main
         $this->setSlim();
     }
 
-    public function run()
-    {
-        $this->slim->run();
-    }
-
     private function configuration()
     {
         $this->web = Configuration::getInstance();
+        $this->setRoutes($this->web->get('slim')['rules']);
+
+    }
+
+    private function createContainer() : Container
+    {
+        $options = [];
+        $options['settings'] = [
+            'displayErrorDetails' => $this->web->get('debug')
+        ];
+
+        return new Container($options);
+    }
+
+    private function createView()
+    {
+        $container = $this->getContainer();
+        $container['view'] = function (Container $container) {
+            $router = $container->router;
+            $callbacks = [];
+            $callbacks['pathFor'] = function ($name, $data = [], $queryParams = []) use ($router) {
+                return $router->pathFor($name, $data, $queryParams);
+            };
+            return new View(VIEW_DIR, $callbacks);
+        };
+
+
     }
 
     private function dbconnect()
@@ -78,11 +108,28 @@ class Main
         require_once ROOT_DIR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
     }
 
+    private function setRoutes(array $routes)
+    {
+        foreach ($routes as $controllers) {
+            if (isset($controllers['groups'])) {
+                $this->group($controllers['pattern'], function () use ($controllers) {
+                    foreach ($controllers['groups'] as $controller) {
+                        $this->map($controller['methods'], $controller['pattern'], $controller['action'])
+                            ->setName($controller['name']);
+                    }
+                });
+            } else {
+                $this->map($controllers['methods'], $controllers['pattern'], $controllers['action'])
+                    ->setName($controllers['name']);
+            }
+        }
+    }
+
     private function setSlim()
     {
         $config = new SlimConfig($this->web->slim);
 
-        $this->slim   = $config->getSlim();
+        $this->slim = $config->getSlim();
         $this->router = $config->getRouter();
     }
 }
