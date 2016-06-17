114e036c254241de64a5b80bf300862c649ecae8
xandros15 <xandros92@gmail.com>
2016-06-17 19:14:09 +0200

    flat array

--- src/config/web.php
+++ src/config/web.php
@@ -9,94 +9,90 @@ return [
     'siteUrl' => 'http://aigisu.pl',
     'locale' => 'en',
     'debug' => false,
-    'slim' => [
-        'values' => [
-            'settings' => [
-                'displayErrorDetails' => true,
-            ],
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+    'controllers' => [
+        ImageController::class,
+        ImageFileController::class,
+        UnitController::class,
+        OauthController::class,
+    ],
+    'routes' => [
+        [
+            'pattern' => '/',
+            'methods' => ['get'],
+            'action' => UnitController::class . ':actionIndex',
+            'name' => 'home'
         ],
-        'controllers' => [
-            ImageController::class,
-            ImageFileController::class,
-            UnitController::class,
-            OauthController::class,
+        [
+            'pattern' => '/image',
+            'group' => [
+                [
+                    'pattern' => '/upload/{id:\d+}',
+                    'methods' => ['post'],
+                    'action' => ImageFileController::class . ':actionCreate',
+                    'name' => 'imageUpload'
+                ],
+                [
+                    'pattern' => '/{id:\d+}',
+                    'methods' => ['get'],
+                    'action' => ImageController::class . ':actionIndex',
+                    'name' => 'image'
+                ]
+            ]
         ],
-        'routes' => [
-            [
-                'pattern' => '/',
-                'methods' => ['get'],
-                'action' => UnitController::class . ':actionIndex',
-                'name' => 'home'
-            ],
-            [
-                'pattern' => '/image',
-                'group' => [
-                    [
-                        'pattern' => '/upload/{id:\d+}',
-                        'methods' => ['post'],
-                        'action' => ImageFileController::class . ':actionCreate',
-                        'name' => 'imageUpload'
-                    ],
-                    [
-                        'pattern' => '/{id:\d+}',
-                        'methods' => ['get'],
-                        'action' => ImageController::class . ':actionIndex',
-                        'name' => 'image'
-                    ]
+        [
+            'pattern' => '/unit',
+            'group' => [
+                [
+                    'pattern' => '[/]',
+                    'methods' => ['get'],
+                    'action' => UnitController::class . ':actionIndex',
+                    'name' => 'unit'
+                ],
+                [
+                    'pattern' => '/update/{id:\d+}',
+                    'methods' => ['post'],
+                    'action' => UnitController::class . ':actionUpdate',
+                    'name' => 'unitUpdate'
+                ],
+                [
+                    'pattern' => '/create',
+                    'methods' => ['post'],
+                    'action' => UnitController::class . ':actionCreate',
+                    'name' => 'unitCreate'
+                ],
+                [
+                    'pattern' => '/delete/{id:\d+}',
+                    'methods' => ['get'],
+                    'action' => UnitController::class . ':actionDelete',
+                    'name' => 'unitDelete'
                 ]
-            ],
-            [
-                'pattern' => '/unit',
-                'group' => [
-                    [
-                        'pattern' => '[/]',
-                        'methods' => ['get'],
-                        'action' => UnitController::class . ':actionIndex',
-                        'name' => 'unit'
-                    ],
-                    [
-                        'pattern' => '/update/{id:\d+}',
-                        'methods' => ['post'],
-                        'action' => UnitController::class . ':actionUpdate',
-                        'name' => 'unitUpdate'
-                    ],
-                    [
-                        'pattern' => '/create',
-                        'methods' => ['post'],
-                        'action' => UnitController::class . ':actionCreate',
-                        'name' => 'unitCreate'
-                    ],
-                    [
-                        'pattern' => '/delete/{id:\d+}',
-                        'methods' => ['get'],
-                        'action' => UnitController::class . ':actionDelete',
-                        'name' => 'unitDelete'
-                    ]
+            ]
+        ],
+        [
+            'pattern' => '/oauth',
+            'group' => [
+                [
+                    'pattern' => '[/]',
+                    'methods' => ['get'],
+                    'action' => OauthController::class . ':actionIndex',
+                    'name' => 'oauth'
+                ],
+                [
+                    'pattern' => '/login',
+                    'methods' => ['post'],
+                    'action' => OauthController::class . ':actionLogin',
+                    'name' => 'login'
+                ],
+                [
+                    'pattern' => '/logout',
+                    'methods' => ['post'],
+                    'action' => OauthController::class . ':actionLogout',
+                    'name' => 'logout'
                 ]
-            ],
-            [
-                'pattern' => '/oauth',
-                'group' => [
-                    [
-                        'pattern' => '[/]',
-                        'methods' => ['get'],
-                        'action' => OauthController::class . ':actionIndex',
-                        'name' => 'oauth'
-                    ],
-                    [
-                        'pattern' => '/login',
-                        'methods' => ['post'],
-                        'action' => OauthController::class . ':actionLogin',
-                        'name' => 'login'
-                    ],
-                    [
-                        'pattern' => '/logout',
-                        'methods' => ['post'],
-                        'action' => OauthController::class . ':actionLogout',
-                        'name' => 'logout'
-                    ]
-                ]
-            ],
+            ]
         ],
     ],
 ];
