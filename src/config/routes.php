397e6a1111a63ca05ac7ff2fc4ab784074396471
xandros15 <xandros92@gmail.com>
2016-06-18 17:53:23 +0200

    more flexable router

--- src/config/routes.php
+++ src/config/routes.php
@@ -5,3 +5,29 @@
  * Date: 2016-06-18
  * Time: 17:00
  */
+
+use controller\ImageController;
+use controller\ImageFileController;
+use controller\OauthController;
+use controller\UnitController;
+
+/** @var $this \Aigisu\Main */
+$this->map(['get'], '/', UnitController::class . ':actionIndex')->setName('home');
+$this->group('/image', function () {
+    /** @var $this \Aigisu\Main */
+    $this->map(['post'], '/upload/{id:\d+}', ImageFileController::class . ':actionCreate')->setName('imageUpload');
+    $this->map(['get'], '/{id:\d+}', ImageController::class . ':actionIndex')->setName('image');
+});
+$this->group('/unit', function () {
+    /** @var $this \Aigisu\Main */
+    $this->map(['get'], '[/]', UnitController::class . ':actionIndex')->setName('unit');
+    $this->map(['post'], '/update/{id:\d+}', UnitController::class . ':actionUpdate')->setName('unitUpdate');
+    $this->map(['post'], '/create', UnitController::class . ':actionCreate')->setName('unitCreate');
+    $this->map(['get'], '/delete/{id:\d+}', UnitController::class . ':actionDelete')->setName('unitDelete');
+});
+$this->group('/oauth', function () {
+    /** @var $this \Aigisu\Main */
+    $this->map(['get'], '[/]', OauthController::class . ':actionIndex')->setName('oauth');
+    $this->map(['post'], '/login', OauthController::class . ':actionLogin')->setName('login');
+    $this->map(['post'], '/logout', OauthController::class . ':actionLogout')->setName('logout');
+});
