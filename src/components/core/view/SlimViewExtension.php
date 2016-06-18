9cfb26f2aec605d7d04bd03e85d39ba33a696d6c
xandros15 <xandros92@gmail.com>
2016-06-18 03:44:44 +0200

    add methods to extension

--- src/components/core/view/SlimViewExtension.php
+++ src/components/core/view/SlimViewExtension.php
@@ -17,6 +17,12 @@ class SlimViewExtension extends ViewExtension
     public function __construct(Container $container)
     {
         $this->container = $container;
+        $this->init();
+    }
+
+    public function init()
+    {
+        $this->addCallback([$this, 'pathFor']);
     }
 
     public function __get($name)
@@ -25,6 +31,11 @@ class SlimViewExtension extends ViewExtension
             return $this->container->get($name);
         }
         throw new \InvalidArgumentException();
+    }
+
+    public function pathFor($name, $data = [], $queryParams = [])
+    {
+        return $this->router->pathFor($name, $data, $queryParams);
     }
 
     public function getName() : string
