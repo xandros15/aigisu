fb893d5236a2256871e9d2e58da836118de38845
xandros15 <xandros92@gmail.com>
2016-07-11 02:42:01 +0200

    Core: init passing model

---
+++ src/components/core/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-07-11
+ * Time: 02:03
+ */
+
+namespace Aigisu;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class Model extends EloquentModel
+{
+}
