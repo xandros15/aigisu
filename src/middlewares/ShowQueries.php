fb5fe2ca78906c86d8187894dc6be452ad18ef0c
xandros15 <xandros92@gmail.com>
2016-08-06 03:38:05 +0200

    Database: add query before body ends if exist

--- src/middlewares/ShowQueries.php
+++ src/middlewares/ShowQueries.php
@@ -12,6 +12,8 @@ namespace Middlewares;
 use Aigisu\Middleware;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 class ShowQueries extends Middleware
 {
@@ -19,9 +21,30 @@ class ShowQueries extends Middleware
     public function __invoke(Request $request, Response $response, callable $next)
     {
         $this->connection->enableQueryLog();
+        /** @var $response Response */
         $response = $next($request, $response);
-        dump($this->connection->getQueryLog());
 
-        return $response;
+        return $response->withBody($this->getBodyWithNewContent($this->getQueryContent(), $response));
+    }
+
+    private function getBodyWithNewContent($newContent, Response $response)
+    {
+        $body = $response->getBody();
+        $body->rewind();
+        $content   = $body->getContents();
+        $endOfBody = strpos($content, '</body>');
+        $body->seek($endOfBody !== false ? $endOfBody : strlen($content));
+        $body->write($newContent);
+
+        return $body;
+    }
+
+    private function getQueryContent()
+    {
+        $output = fopen('php://memory', 'r+b');
+        $dumper = new HtmlDumper();
+        $dumper->dump((new VarCloner())->cloneVar($this->connection->getQueryLog()), $output);
+
+        return stream_get_contents($output, -1, 0);
     }
 }
