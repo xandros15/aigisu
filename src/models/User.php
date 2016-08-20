a3f8a64b6d3318ed0858cd4985f793bc27f47008
xandros15 <xandros92@gmail.com>
2016-08-20 23:56:56 +0200

    #20 add simple password encryption

--- src/models/User.php
+++ src/models/User.php
@@ -11,15 +11,37 @@ namespace Models;
 
 use Aigisu\Model;
 
+/**
+ * @property string $name
+ * @property string $password_hash
+ * @property string $email
+ * @property string $access_token
+ * @property string $recovery_hash
+ * @property string $remember_identifier
+ * @property string $remember_hash
+ */
 class User extends Model
 {
+    public $password;
     protected $fillable = [
         'name',
-        'password',
+        'password_hash',
         'email',
         'access_token',
         'recovery_hash',
         'remember_identifier',
         'remember_hash',
     ];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->password = $attributes['password'] ?? '';
+        $this->setEncryptPassword($this->password);
+        parent::__construct($attributes);
+    }
+
+    private function setEncryptPassword(string $password)
+    {
+        $this->password_hash = password_hash($password, PASSWORD_DEFAULT);
+    }
 }
