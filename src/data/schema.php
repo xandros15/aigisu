86bd0bc8c6107d21e7cfaca5ea89ac1a9efdd035
xandros15 <xandros92@gmail.com>
2016-08-20 12:18:26 +0200

    #21 #1 disable foreign keys on init schema

--- src/data/schema.php
+++ src/data/schema.php
@@ -8,6 +8,9 @@
 namespace Aigisu;
 
 use Illuminate\Database\Schema\Blueprint;
+use Models\Image;
+use Models\Tag;
+use Models\Unit;
 use Models\User;
 
 require_once '../../vendor/autoload.php';
@@ -20,9 +23,17 @@ $database->bootEloquent();
 
 $connection = $database->getConnection();
 $builder = $connection->getSchemaBuilder();
+$builder->disableForeignKeyConstraints();
 $createTable = function (string $table, \Closure $schema) use ($builder) {
     $builder->dropIfExists($table);
     $builder->create($table, $schema);
+};
+
+$pivot = function (Model $firstModel, Model $secondModel) : string {
+    $fistTable = rtrim($firstModel->getTable(), 's');
+    $secondTable = rtrim($secondModel->getTable(), 's');
+
+    return ($fistTable <=> $secondTable) === -1 ? $fistTable . '_' . $secondTable : $secondTable . '_' . $fistTable;
 };
 
 $createTable((new User())->getTable(), function (Blueprint $table) {
@@ -39,4 +50,63 @@ $createTable((new User())->getTable(), function (Blueprint $table) {
     $table->string('remember_identifier', 255)->nullable();
     $table->string('remember_hash', 255)->nullable();
     $table->timestamps();
+});
+
+$createTable((new Unit())->getTable(), function (Blueprint $table) {
+    $table->collation = 'utf8mb4_unicode_ci';
+    $table->charset = 'utf8mb4';
+    $table->engine = 'InnoDB';
+
+    $table->increments('id')->unsigned();
+    $table->string('name', 25);
+    $table->string('original', 45);
+    $table->string('icon', 100);
+    $table->string('link', 100)->nullable();
+    $table->string('linkgc', 100)->nullable();
+    $table->enum('rarity', Unit::getRarities());
+    $table->boolean('is_male');
+    $table->boolean('is_only_dmm');
+    $table->boolean('has_aw_image');
+    $table->timestamps();
+});
+
+$createTable((new Image())->getTable(), function (Blueprint $table) {
+    $table->collation = 'utf8mb4_unicode_ci';
+    $table->charset = 'utf8mb4';
+    $table->engine = 'InnoDB';
+
+    $table->increments('id')->unsigned();
+    $table->string('md5', 32);
+    $table->integer('unit_id', false, true);
+    $table->enum('server', Image::getServersNames());
+    $table->tinyInteger('scene', false, true);
+    $table->string('google_id', 64);
+    $table->string('imgur_id', 64);
+    $table->string('imgur_delhash', 64);
+    $table->timestamps();
+
+    $table->foreign('unit_id')->references('id')->on((new Unit())->getTable())->onDelete('cascade');
+});
+
+$createTable((new Tag())->getTable(), function (Blueprint $table) {
+    $table->collation = 'utf8mb4_unicode_ci';
+    $table->charset = 'utf8mb4';
+    $table->engine = 'InnoDB';
+
+    $table->increments('id')->unsigned();
+    $table->string('name', 25);
+    $table->timestamps();
+});
+
+$unitToTag = $pivot(new Unit(), new Tag());
+$createTable($unitToTag, function (Blueprint $table) {
+    $table->collation = 'utf8mb4_unicode_ci';
+    $table->charset = 'utf8mb4';
+    $table->engine = 'InnoDB';
+
+    $table->increments('id')->unsigned();
+    $table->integer('unit_id')->unsigned();
+    $table->integer('tag_id')->unsigned();
+    $table->foreign('unit_id')->references('id')->on((new Unit())->getTable())->onDelete('cascade');
+    $table->foreign('tag_id')->references('id')->on((new Tag())->getTable())->onDelete('cascade');
 });
