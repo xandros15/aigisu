85e9f6aef47bb21146ab6de489bb72bfb6b4a91e
xandros15 <xandros92@gmail.com>
2016-08-21 12:25:45 +0200

    Refactor core: make fill more flex

--- src/components/core/Model.php
+++ src/components/core/Model.php
@@ -8,6 +8,7 @@
 
 namespace Aigisu;
 
+use Illuminate\Database\Eloquent\MassAssignmentException;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 /**
@@ -17,4 +18,51 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
  */
 class Model extends EloquentModel
 {
+    /**
+     * Fill the model with an array of attributes.
+     *
+     * @param  array $attributes
+     * @return $this
+     *
+     * @throws \Illuminate\Database\Eloquent\MassAssignmentException
+     */
+    public function fill(array $attributes)
+    {
+        $totallyGuarded = $this->totallyGuarded();
+
+        $fillable = $this->fillableFromArray($attributes);
+        $this->setProperties(array_diff($attributes, $fillable));
+
+        foreach ($fillable as $key => $value) {
+            $key = $this->removeTableFromKey($key);
+
+            // The developers may choose to place some attributes in the "fillable"
+            // array, which means only those attributes may be set through mass
+            // assignment to the model, and all others will just be ignored.
+            if ($this->isFillable($key)) {
+                $this->setAttribute($key, $value);
+            } elseif ($totallyGuarded) {
+                throw new MassAssignmentException($key);
+            }
+        }
+
+
+        return $this;
+    }
+
+    /**
+     * @param array $properties
+     * @return $this
+     */
+    protected function setProperties(array $properties)
+    {
+        foreach ($properties as $key => $value) {
+            $key = $this->removeTableFromKey($key);
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+
+        return $this;
+    }
 }
