1ec609344d7a3fcabd841263bae67aebfbaeb9fa
xandros15 <xandros92@gmail.com>
2016-08-22 19:50:11 +0200

    #22: Copy controllers as api controllers

---
+++ src/api/controllers/UnitController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Api\Controllers;
+
+use Aigisu\Alert\Alert;
+use Aigisu\Controller;
+use Models\Unit;
+use Models\UnitSort;
+use Slim\Http\Request;
+use Xandros15\SlimPagination\Pagination;
+
+class UnitController extends Controller
+{
+
+    public function actionIndex(Request $request)
+    {
+        $unitSort = new UnitSort($request, $this->router);
+        $unitSearch = Unit::with(['images', 'tags']);
+        $max = $unitSearch->count();
+        $unitSearch->forPage($request->getParam('page', 1), Unit::UNITS_PER_PAGE);
+        foreach ($unitSort->getOrders() as $order => $direction) {
+            $unitSearch->orderBy($order, $direction);
+        }
+        $list = $unitSearch->get();
+
+        return $this->render('unit/index', [
+            'unitList' => $list,
+            'pagination' => new Pagination($request, $this->router, [
+                Pagination::OPT_TOTAL => $max,
+                Pagination::OPT_PER_PAGE => Unit::UNITS_PER_PAGE
+            ]),
+            'unitSort' => $unitSort
+        ]);
+    }
+
+    public function actionView(Request $request)
+    {
+        $unit = Unit::firstOrNew(['id' => $request->getAttribute('id')]);
+
+        return $this->render('unit/view', ['unit' => $unit]);
+    }
+
+    public function actionCreate(Request $request)
+    {
+        $unit = new Unit($request->getParams());
+        if ($request->isPost()) {
+            if ($unit->validate() && $unit->save()) {
+                Alert::add('Successful added ' . $unit->name);
+                return $this->goBack();
+            }
+        }
+
+        return $this->render('unit/view', ['unit' => $unit]);
+    }
+
+    public function actionUpdate(Request $request)
+    {
+        /* @var $unit Unit */
+        $unit = Unit::find($request->getAttribute('id'));
+
+        if ($request->isPost()) {
+            $unit->addTagsToUnit($request->getParam('tags'));
+            if ($unit->fill($request->getParams())->validate() && $unit->save()) {
+                Alert::add("Successful update {$unit->name}");
+                return $this->goBack();
+            }
+        }
+
+        return $this->render('unit/view', ['unit' => $unit]);
+    }
+
+    public function actionDelete(Request $request)
+    {
+        /* @var $model Unit */
+        $model = Unit::find($request->getAttribute('id'));
+
+        if ($model->delete()) {
+            Alert::add("Successful delete {$model->name}");
+        }
+
+        return $this->goBack();
+    }
+
+    public function actionShowImages(Request $request)
+    {
+        $unit = Unit::find($request->getAttribute('id'));
+        return $this->render('image/index', ['unit' => $unit]);
+    }
+}
---
+++ src/api/controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-08-19
+ * Time: 23:36
+ */
+
+namespace Api\Controllers;
+
+
+use Aigisu\Alert\Alert;
+use Aigisu\Controller;
+use Models\User;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class UserController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('user/index');
+    }
+
+    public function actionView()
+    {
+        return $this->render('user/view');
+    }
+
+    public function actionCreate(Request $request, Response $response)
+    {
+        $user = new User([
+            'name' => $request->getParam('name'),
+            'email' => $request->getParam('email'),
+            'password_hash' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+        ]);
+
+        if ($request->isPost() && $user->validate() && $user->save()) {
+            Alert::add("Created {$user->name} user");
+            return $response->withRedirect('/users');
+        }
+
+        return $this->render('/auth/register', ['user' => $user]);
+    }
+
+    public function actionUpdate()
+    {
+        if ($this->request->isPost()) {
+            //@todo redirect to updated user
+            return $this->response->withRedirect('/users');
+        }
+
+        return $this->render('/user/edit');
+    }
+
+    public function actionDelete()
+    {
+        return $this->response->withRedirect('/users');
+    }
+}
