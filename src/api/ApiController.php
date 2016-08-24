e3b917029c48b1794cf37e0b0d7bcf0a4ab13833
xandros15 <xandros92@gmail.com>
2016-08-24 20:47:53 +0200

    #22 Add rest of status codes

--- src/api/ApiController.php
+++ src/api/ApiController.php
@@ -17,8 +17,10 @@ class ApiController extends Controller
     const
         STATUS_OK = 200,
         STATUS_CREATED = 201,
-        STATUS_NOT_FOUND = 404,
         STATUS_BAD_REQUEST = 400,
+        STATUS_UNAUTHORIZED = 401,
+        STATUS_FORBIDDEN = 403,
+        STATUS_NOT_FOUND = 404,
+        STATUS_METHOD_NOT_ALLOWED = 405,
         STATUS_SERVER_ERROR = 500;
-
 }
