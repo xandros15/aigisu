97f5a7bcd9b98701201b3c20018666875c53bc68
xandros15 <xandros92@gmail.com>
2016-08-24 11:01:12 +0200

    Create validator as middleware

---
+++ src/middlewares/Validator.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xandros15
+ * Date: 2016-08-24
+ * Time: 10:31
+ */
+
+namespace Middlewares;
+
+
+use Aigisu\Alert\Alert;
+use Aigisu\Middleware;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+abstract class Validator extends Middleware
+{
+    const OLD_PARAMS = 'old';
+    const ERRORS = 'errors';
+    /**
+     * @var array
+     */
+    protected $errors;
+
+    public function __invoke(Request $request, Response $response, callable $next) : Response
+    {
+        if ($this->validate($request)) {
+            return $next($request, $response);
+        }
+
+        return $response->withJson([
+            self::ERRORS => $this->getErrors(),
+            self::OLD_PARAMS => $request->getParams()
+        ], 400);
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public function validate(Request $request)
+    {
+        foreach ($this->rules() as $field => $rule) {
+            try {
+                /** @var $rule \Respect\Validation\Validator */
+                $rule->setName(ucfirst($field))->assert($request->getParam($field));
+            } catch (NestedValidationException $exception) {
+                $this->errors[$field] = $exception->getMessages();
+                Alert::add($exception->getFullMessage(), Alert::ERROR);
+            }
+        }
+
+        return !$this->errors;
+    }
+
+    /**
+     * @return array
+     */
+    abstract protected function rules() : array;
+
+    /**
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+}
