85798b8b9fe3b7d6f7b19905ee15aa4660e193ee
xandros15 <xandros92@gmail.com>
2016-08-26 17:29:35 +0200

    #25: better retrieve models

--- src/api/controllers/Controller.php
+++ src/api/controllers/Controller.php
@@ -11,7 +11,20 @@ namespace Aigisu\Api\Controllers;
 
 use Aigisu\Api\Messages;
 use Aigisu\Core\ActiveContainer;
+use Slim\Http\Request;
 
 class Controller extends ActiveContainer implements Messages
 {
+    const EXTENDED = 'extended';
+    const INDEX = 'id';
+
+    protected function getExtendedParam(Request $request)
+    {
+        return $request->getQueryParam(self::EXTENDED, []);
+    }
+
+    protected function getID(Request $request) : int
+    {
+        return $request->getAttribute(self::INDEX, -1);
+    }
 }
--- src/api/controllers/UnitController.php
+++ src/api/controllers/UnitController.php
@@ -11,14 +11,14 @@ class UnitController extends Controller
 
     public function actionIndex(Request $request, Response $response)
     {
-        $units = Unit::with(['images', 'tags']);
+        $units = Unit::with($this->getExtendedParam($request))->get();
 
-        return $response->withJson($units->get()->toArray(), self::STATUS_OK);
+        return $response->withJson($units->toArray(), self::STATUS_OK);
     }
 
     public function actionView(Request $request, Response $response)
     {
-        $unit = Unit::with(['images', 'tags'])->findOrFail($request->getAttribute('id'));
+        $unit = Unit::with($this->getExtendedParam($request))->findOrFail($this->getID($request));
 
         return $response->withJson($unit->toArray(), self::STATUS_OK);
     }
@@ -28,23 +28,25 @@ class UnitController extends Controller
         $unit = new Unit($request->getParams());
 
         $unit->saveOrFail();
+        $unit->load($this->getExtendedParam($request));
 
-        return $response->withJson($unit->load(['images', 'tags'])->toArray(), self::STATUS_CREATED);
+        return $response->withJson($unit->toArray(), self::STATUS_CREATED);
     }
 
     public function actionUpdate(Request $request, Response $response)
     {
-        $unit = Unit::findOrFail($request->getAttribute('id'));
+        $unit = Unit::findOrFail($this->getID($request));
 
         $unit->fill($request->getParams());
-        $unit->saveOrFail();;
+        $unit->saveOrFail();
+        $unit->load($this->getExtendedParam($request));
 
-        return $response->withJson($unit->load(['tags', 'images'])->toArray(), self::STATUS_OK);
+        return $response->withJson($unit->toArray(), self::STATUS_OK);
     }
 
     public function actionDelete(Request $request, Response $response)
     {
-        $unit = Unit::findOrFail($request->getAttribute('id'));
+        $unit = Unit::findOrFail($this->getID($request));
         $unit->delete();
 
         return $response->withJson($unit->toArray(), self::STATUS_OK);
