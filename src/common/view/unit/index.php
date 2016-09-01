94f502bb36a34a7952520ea69ea96273f7e07f71
xandros15 <xandros92@gmail.com>
2016-09-01 21:42:59 +0200

    Refactor: nicer unit index view

--- src/common/view/unit/index.php
+++ src/common/view/unit/index.php
@@ -1,6 +1,7 @@
 <?php
 
-use Aigisu\Api\Models\Unit;
+use Aigisu\Common\Components\View\UrlExtension;
+use Aigisu\Common\Components\View\View;
 use Aigisu\Common\Models\UnitSort;
 use Illuminate\Database\Eloquent\Collection;
 use Xandros15\SlimPagination\Pagination;
@@ -8,37 +9,69 @@ use Xandros15\SlimPagination\Pagination;
 /* @var $units Collection */
 /* @var $pagination Pagination */
 /* @var $unitSort UnitSort */
+/* @var $this UrlExtension | View */
 
 $this->title = 'Units';
-$this->containerClass = 'container';
 /** @var $unitSort string */
 $unitSort = $this->render('unit/sort', ['unitSort' => $unitSort]);
 /** @var $pagination string */
 $pagination = $this->render('unit/pagination', ['pagination' => $pagination]);
 
-$createPath = $this->pathFor('unit.create');
+$units = $units->map(function ($unit) {
+    $applyColumn = function (&$column, $key) {
+        $element = $key === 'id' ? 'th' : 'td';
+        $column = "<$element>{$column}</$element>";
+    };
+    array_walk($unit, function (&$param) {
+        if (is_scalar($param)) {
+            $param = htmlentities($param);
+        }
+    });
+
+    /* @var $this UrlExtension */
+    $iconSrc = $this->pathFor('unit.icon', ['name' => $unit['icon_name']]);
+    $unitPath = $this->pathFor('unit.view', ['id' => $unit['id']]);
+    $params = [
+        '#' => $unit['id'],
+        'Unit' => "<a href='{$unitPath}' >{$unit['name']}</a>",
+        'Icon' => "<img style='max-width:98px;' class='img-responsive' src='{$iconSrc}' alt='{$unit['name']}'>",
+        'Kanji' => $unit['original'],
+        'Links' => "<a class='btn btn-default' style='margin:5px;' href='{$unit['link']}'>seesaw</a>" .
+            "<a class='btn btn-default' style='margin:5px;' href='{$unit['linkgc']}'>gc wiki</a>",
+        'Rarity' => $unit['rarity'],
+        'Created' => "<time>{$unit['created_at']}</time>",
+        'Updated' => "<time>{$unit['updated_at']}</time>",
+    ];
+
+    array_walk($params, $applyColumn);
+    return $params;
+});
+
+$labels = array_keys($units->first());
+array_walk($labels, function (&$label) {
+    $label = "<th>{$label}</th>";
+})
+
 ?>
 <div id="units">
+    <div class="form-group">
+        <?= $this->render('image/help') ?>
+    </div>
     <?php if (!$units->isEmpty()): ?>
-        <div class="form-group">
-            <?= $this->render('image/help') ?>
-            <a type="button" class="btn btn-primary pull-right" data-target="<?= $createPath ?>"
-               href="<?= $createPath ?>">
-                Create
-            </a>
-        </div>
         <nav class="text-center">
             <?= $pagination ?>
         </nav>
-        <ul class="unit-list list-group panel panel-default">
-            <li class="panel-heading list-group-item">
-                <?= $unitSort ?>
-            </li>
+        <?= $unitSort ?>
+        <table class="table table-bordered">
+            <thead>
+            <tr><?= implode("\r\n\t", $labels) ?></tr>
+            </thead>
+            <tbody>
             <?php foreach ($units as $unit): ?>
-                <?php /** @var $unit Unit */ ?>
-                <?= $this->render('unit/unit', ['unit' => $unit]) ?>
+                <tr><?= implode("\r\n\t", $unit) ?></tr>
             <?php endforeach; ?>
-        </ul>
+            </tbody>
+        </table>
         <nav class="text-center col-xs-12">
             <?= $pagination ?>
         </nav>
