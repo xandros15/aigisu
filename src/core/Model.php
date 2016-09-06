9baabf735f56e25f121255460356ab7bfb8f7b88
xandros15 <xandros92@gmail.com>
2016-09-06 15:57:02 +0200

    Hotfix: remove unused function

--- src/core/Model.php
+++ src/core/Model.php
@@ -17,16 +17,4 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
  */
 abstract class Model extends EloquentModel
 {
-    /**
-     * @param array $files
-     * @return $this
-     */
-    public function attachUploadedFiles(array $files)
-    {
-        foreach ($files as $name => $file) {
-            if (method_exists($this, $method = 'attach' . ucfirst($name))) {
-                $this->{$method}($file);
-            }
-        }
-    }
 }
