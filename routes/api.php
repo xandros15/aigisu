6541cbbfa26705bc50cdc6d952f601b58dbd01a9
xandros15 <xandros92@gmail.com>
2016-10-09 18:11:22 +0200

    #18: attach access control to api

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,9 @@ use Aigisu\Api\Controllers\Unit\CG\ImgurUploader;
 use Aigisu\Api\Controllers\Unit\CGController;
 use Aigisu\Api\Controllers\UnitController;
 use Aigisu\Api\Controllers\UserController;
+use Aigisu\Api\Middlewares\Access\AdminAccessMiddleware;
+use Aigisu\Api\Middlewares\Access\ModeratorAccessMiddleware;
+use Aigisu\Api\Middlewares\Access\OwnerAccessMiddleware;
 use Aigisu\Api\Middlewares\CG\ExtendedServerExceptionHandler;
 use Aigisu\Api\Middlewares\ParserUnitTagsMiddleware;
 use Aigisu\Api\Middlewares\Validators\CreateCGValidator;
@@ -20,27 +23,33 @@ use Aigisu\Api\Middlewares\Validators\MissingCGValidator;
 use Aigisu\Api\Middlewares\Validators\UpdateCGValidator;
 use Aigisu\Api\Middlewares\Validators\UpdateUnitValidator;
 use Aigisu\Api\Middlewares\Validators\UpdateUserValidator;
-use League\OAuth2\Server\ResourceServer;
 use League\OAuth2\Server\Middleware\ResourceServerMiddleware;
+use League\OAuth2\Server\ResourceServer;
 
 /** @var $this \Aigisu\Core\Main */
 $this->group('/users', function () {
     /** @var $this \Aigisu\Core\Main */
-    $this->post('', UserController::class . ':actionCreate')
-        ->setName('api.user.create')
-        ->add(new CreateUserValidator($this->getContainer()));
-
     $this->get('', UserController::class . ':actionIndex')
         ->setName('api.user.index');
+
     $this->get('/{id:\d+}', UserController::class . ':actionView')
         ->setName('api.user.view');
 
-    $this->patch('/{id:\d+}', UserController::class . ':actionUpdate')
-        ->setName('api.user.update')
-        ->add(new UpdateUserValidator($this->getContainer()));
+    $this->group('', function () {
+        /** @var $this \Aigisu\Core\Main */
+        $this->post('', UserController::class . ':actionCreate')
+            ->setName('api.user.create')
+            ->add(new CreateUserValidator($this->getContainer()));
 
-    $this->delete('/{id:\d+}', UserController::class . ':actionDelete')
-        ->setName('api.user.delete');
+        $this->patch('/{id:\d+}', UserController::class . ':actionUpdate')
+            ->setName('api.user.update')
+            ->add(new UpdateUserValidator($this->getContainer()));
+
+        $this->delete('/{id:\d+}', UserController::class . ':actionDelete')
+            ->setName('api.user.delete');
+
+    })->add(new OwnerAccessMiddleware($this->getContainer()));
+
 })->add(new ResourceServerMiddleware($this->getContainer()->get(ResourceServer::class)));
 
 $this->group('/units', function () {
@@ -51,17 +60,24 @@ $this->group('/units', function () {
             ->setName('api.unit.create')
             ->add(new CreateUnitValidator($this->getContainer()))
             ->add(new ParserUnitTagsMiddleware($this->getContainer()));
+
         $this->post('/{id:\d+}', UnitController::class . ':actionUpdate')
             ->setName('api.unit.update')
             ->add(new UpdateUnitValidator($this->getContainer()))
-            ->add(new ParserUnitTagsMiddleware($this->getContainer()));
+            ->add(new ParserUnitTagsMiddleware($this->getContainer()))
+            ->add(new AdminAccessMiddleware($this->getContainer()));
+
         $this->delete('/{id:\d+}', UnitController::class . ':actionDelete')
             ->setName('api.unit.delete');
+
     })->add(new ResourceServerMiddleware($this->getContainer()->get(ResourceServer::class)));
+
     $this->get('', UnitController::class . ':actionIndex')
         ->setName('api.unit.index');
+
     $this->get('/{id:\d+}', UnitController::class . ':actionView')
         ->setName('api.unit.view');
+
     $this->get('/rarities', UnitController::class . ':actionRarities')
         ->setName('api.unit.rarities');
 
@@ -73,37 +89,48 @@ $this->group('/units', function () {
                 ->setName('api.unit.cg.create')
                 ->add(new MissingCGValidator($this->getContainer()))
                 ->add(new CreateCGValidator($this->getContainer()));
+
             $this->post('/{id:\d+}', CGController::class . ':actionUpdate')
                 ->setName('api.unit.cg.update')
                 ->add(new MissingCGValidator($this->getContainer()))
                 ->add(new UpdateCGValidator($this->getContainer()));
 
             $this->group('/{id:\d+}/google', function () {
+                /** @var $this \Aigisu\Core\Main */
                 $this->post('', GoogleUploader::class . ':actionCreate')
                     ->setName('api.unit.cg.google.create');
+
                 $this->patch('', GoogleUploader::class . ':actionUpdate')
                     ->setName('api.unit.cg.google.update');
+
                 $this->delete('', GoogleUploader::class . ':actionDelete')
                     ->setName('api.unit.cg.google.delete');
+
             })->add(new ExtendedServerExceptionHandler($this->getContainer()));
 
             $this->group('/{id:\d+}/imgur', function () {
                 $this->post('', ImgurUploader::class . ':actionCreate')
                     ->setName('api.unit.cg.imgur.create');
+
                 $this->patch('', ImgurUploader::class . ':actionUpdate')
                     ->setName('api.unit.cg.imgur.update');
+
                 $this->delete('', ImgurUploader::class . ':actionDelete')
                     ->setName('api.unit.cg.imgur.delete');
+
             })->add(new ExtendedServerExceptionHandler($this->getContainer()));
+
             $this->delete('/{id:\d+}', CGController::class . ':actionDelete')
                 ->setName('api.unit.cg.delete');
-        })->add(new ResourceServerMiddleware($this->getContainer()->get(ResourceServer::class)));
+
+        })->add(new ModeratorAccessMiddleware($this->getContainer()))
+            ->add(new ResourceServerMiddleware($this->getContainer()->get(ResourceServer::class)));
 
         $this->get('', CGController::class . ':actionIndex')
             ->setName('api.unit.cg.index');
+
         $this->get('/{id:\d+}', CGController::class . ':actionView')
             ->setName('api.unit.cg.view');
-
 
     });
 });
