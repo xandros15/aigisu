09ad9003aa2696b7cafff2ce741362c34ac28edc
xandros15 <xandros92@gmail.com>
2016-11-19 19:01:58 +0100

    Init index for units page

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,14 @@
 <?php
 //silence is golden
+
+/** @var $this \Aigisu\Core\Main */
+
+use Aigisu\Web\Controllers\UnitController;
+
+$this->get('[/]', function () {
+    list(, $response) = func_get_args();
+    /** @var $response \Slim\Http\Response */
+    return $response->withRedirect('/units');
+});
+
+$this->get('/units', UnitController::class . ':actionIndex');
--- src/web/controllers/UnitController.php
+++ src/web/controllers/UnitController.php
@@ -9,7 +9,22 @@
 namespace Aigisu\Web\Controllers;
 
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\Twig;
+
 class UnitController extends Controller
 {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function actionIndex(Request $request, Response $response) : Response
+    {
+        /** @var $view Twig */
+        $view = $this->get(Twig::class);
 
+        return $view->render($response, 'unit/index.twig');
+    }
 }
