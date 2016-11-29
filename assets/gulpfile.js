5bda67ab5beb8572a4995811a72a74d18688e7c3
xandros15 <xandros92@gmail.com>
2016-11-29 16:21:33 +0100

    change webpack to gulp and clean packages

---
+++ assets/gulpfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xandros15 on 2016-11-26.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function () {
+});
