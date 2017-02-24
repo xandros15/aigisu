89f6e9412a07b6598b04857e16082b799d944c83
xandros15 <xandros92@gmail.com>
2017-02-24 01:34:17 +0100

    add redirection helper function

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,13 @@
 
 /** @var $this \Slim\App */
 
+use Aigisu\Web\Controllers\Controller;
 use Aigisu\Web\Controllers\UnitController;
 
 $this->get('[/]', function () {
     list(, $response) = func_get_args();
     /** @var $response \Slim\Http\Response */
     return $response->withRedirect('/units');
-});
+})->setName(Controller::HOME_PATH_NAME);
 
 $this->get('/units', UnitController::class . ':actionIndex')->setName('web.units');
--- src/Web/Controllers/Controller.php
+++ src/Web/Controllers/Controller.php
@@ -10,8 +10,21 @@ namespace Aigisu\Web\Controllers;
 
 
 use Aigisu\Core\ActiveContainer;
+use Slim\Http\Response;
 
 abstract class Controller extends ActiveContainer
 {
+
+    const HOME_PATH_NAME = 'web.home';
+
+    /**
+     * @param Response $response
+     * @return Response
+     */
+    public function goHome(Response $response) : Response
+    {
+        $path = $this->get('router')->pathFor(self::HOME_PATH_NAME);
+        return $response->withRedirect($path);
+    }
 
 }
